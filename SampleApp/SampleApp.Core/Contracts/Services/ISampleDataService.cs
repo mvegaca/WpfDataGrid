fe74974dcd3e85c13cf04422389ed2bd49b41b85
Martin Vega <mvegaca@outlook.com>
2020-07-28 08:37:05 +0200

    Add out of the box generated app

---
+++ SampleApp/SampleApp.Core/Contracts/Services/ISampleDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using SampleApp.Core.Models;
+
+namespace SampleApp.Core.Contracts.Services
+{
+    public interface ISampleDataService
+    {
+        Task<IEnumerable<SampleOrder>> GetMasterDetailDataAsync();
+    }
+}
---
+++ SampleApp/SampleApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using SampleApp.Helpers;
+
+namespace SampleApp.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
